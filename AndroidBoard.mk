ac73332480bc6efcd27b7dbc8e2fb378a134de49
Kenneth R. Crudup <kenny@panix.com>
2012-07-31 10:57:54 -0700

    Remove kernel copy
    Kernel functions are now in .../build/core/tasks/kernel.mk
    
    Change-Id: I598078b06e8def7d2c1ad322e29127db7016aca8

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,13 +5,6 @@ ifeq ($(TARGET_PREBUILT_KERNEL),)
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 endif
 
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
-
-include $(CLEAR_VARS)
-
 COMMON_DIR := vendor/nvidia/common/
 
 ifeq ($(wildcard $(COMMON_DIR)/TegraBoard.mk),$(COMMON_DIR)/TegraBoard.mk)
